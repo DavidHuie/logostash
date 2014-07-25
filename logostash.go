e4a72adeb7e2eda77a6d875f893c918e3742d7d1
David Huie <david@nationbuilder.com>
2014-07-25 13:48:03 -0700

    Add error for missing connection

--- logostash.go
+++ logostash.go
@@ -5,8 +5,13 @@ package logostash
 import (
 	"bytes"
 	"encoding/json"
+	"errors"
 	"io"
 	"net"
+)
+
+var (
+	MissingConnection = errors.New("Connection is missing")
 )
 
 type Client struct {
@@ -23,6 +28,9 @@ func NewClient(network, address string) (*Client, error) {
 
 // JSON encodes the input struct and sends it to Logstash.
 func (c *Client) SendJson(j interface{}) error {
+	if c.Conn == nil {
+		return MissingConnection
+	}
 	buf := &bytes.Buffer{}
 	encoder := json.NewEncoder(buf)
 	if err := encoder.Encode(j); err != nil {
